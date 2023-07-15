b59f0116a7867fa6aa1edeb380f0e8343de2c993
Andy Godoy <Andygodoy@ymail.com>
2023-07-14 20:49:21 -0700

    added more comments, adjusted syntax and created ReadMe

--- README.md
+++ README.md
@@ -1,106 +1,55 @@
-# WeatherApp_Project-
-This is a 5 day weather forecast application project 
+# Five Day Weather Forecast 
 
+This is a weather dashboard application that allows travelers to view the weather outlook for multiple cities. The application provides current and future weather conditions for a city, helping travelers plan their trips accordingly.
 
-# 06 Server-Side APIs: Weather Dashboard
+## User Stories
 
-## Your Task
+As a traveler, I want to see the weather outlook for multiple cities.
+As a traveler, I want to be able to search for a city and see its current and future conditions.
+As a traveler, I want to have a search history of cities I've previously searched for.
+As a traveler, I want to be able to click on a city in the search history and view its current and future conditions.
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+## Features
 
-Use the [5 Day Weather Forecast](https://openweathermap.org/forecast5) to retrieve weather data for cities. The base URL should look like the following: `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate.
+Search: Users can enter the name of a city in the search input to retrieve weather information.
+Current Conditions: When a user searches for a city, they are presented with the city name, date, an icon representation of weather conditions, temperature, humidity, and wind speed.
+Future Conditions: Users can view a 5-day forecast that displays the date, an icon representation of weather conditions, temperature, wind speed, and humidity.
+Search History: The application keeps track of the cities that the user has searched for, allowing them to click on a city in the search history and view its current and future conditions.
 
-**Hint**: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
+## How to use
 
-You will use `localStorage` to store any persistent data. For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys).
+Open the Weather Dashboard application.
+Enter the name of a city in the search input and click on the search button or press enter.
+The current weather conditions for the searched city will be displayed, including the city name, date, weather icon, temperature, humidity, and wind speed.
+Scroll down to view the 5-day forecast for the searched city, showing the date, weather icon, temperature, wind speed, and humidity.
+The searched city will be added to the search history section.
+To view the current and future conditions for a city from the search history, click on the city name in the search history list.
 
-## User Story
+## Technologies Used
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
+The Weather Dashboard application is built using the following technologies:
 
-## Acceptance Criteria
+HTML
+CSS
+JavaScript
+OpenWeather API (or any other weather API)
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-```
+## Installation
 
-## Mock-Up
+To use the Weather Dashboard application locally, follow these steps:
 
-The following image shows the web application's appearance and functionality:
+Clone the repository: git clone <repository-url>
+Navigate to the project directory.
+Open the index.html file in a web browser.
 
-![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
+## Future Improvements 
 
-## Grading Requirements
+Adding a feature to display additional weather details like precipitation, UV index, or visibility.
+Implementing a geolocation feature to automatically fetch the weather for the user's current location.
+Providing more interactive visualizations for weather data, such as graphs or charts.
+Allowing users to customize the units of measurement (e.g., Celsius or Fahrenheit).
 
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
+## Contributors
 
-This Challenge is graded based on the following criteria: 
+This 5 Day Forecast App was created by Andy Godoy. Contributions and suggestions are welcome. Feel free to fork the repository and submit a pull request with your changes.
 
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-    * Uses the OpenWeather API to retrieve weather data.
-
-    * Uses `localStorage` to store persistent data.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the Challenge instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a readme describing the project.
-
-- - -
-© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
--- assets/script.js
+++ assets/script.js
@@ -1,165 +1,264 @@
  // Global variables
  // Made my APIKey a variable
- var APIKey = "7700e19d13089f026d6e1e644f2228fe";
+var APIKey = "7700e19d13089f026d6e1e644f2228fe";
 
  // decalared city without initializing it
- var city;
+var city;
 
  // create a function and initialized an emtpy array variable to store search history 
- var searchHistory = [];
- 
+var searchHistory = [];
+
  // Function to handle form submission and initiate weather search
  // created function called search that takes an event object as a parameter
  // Submit the form when a search is initiated 
- function search(event) {
+function search(event) {
    event.preventDefault(); // Prevents the default form submission value
 
-   //retrieves the value entered in the inout field (;searchInput') and assigns it to the 'input variable
-   var input = document.getElementById("searchInput").value;
+// retrieves the value entered in the inout field (;searchInput') and assigns it to the 'input variable
+  var input = document.getElementById("searchInput").value;
 
-  //  assigning the value 'input' to the variable 'city'
-   city = input;
- 
-   // GET the DOM element with the id ' search-history' and assigned it to the variable 'searchHistoryContainer'
-   var searchHistoryContainer = document.getElementById("search-history");
+// assigning the value 'input' to the variable 'city'
+  city = input;
 
-   // adds the input value to the beginning of the search history array 
-   searchHistory.unshift(input);
- 
-   // checking the length of the 'searchHistory' is greater than 5
-   if (searchHistory.length > 5) {
+// GET the DOM element with the id ' search-history' and assigned it to the variable 'searchHistoryContainer'
+  var searchHistoryContainer = document.getElementById("search-history");
 
-    
-     //  Removes the last element from the searchHistory array if its length is greater than 5, 
-     // keeping only the most recent 5 search entries.
-     searchHistory.pop();
-   }
- 
-  //  Created a new button element and assigned it to the var 'searchHistoryEntry'
-   var searchHistoryEntry = document.createElement("button");
+// adds the input value to the beginning of the searchHistory array 
+  searchHistory.unshift(input);
 
-   // Setting text content of the searchHistoryEntry button to the input value
-   searchHistoryEntry.innerText = input;
+// checking the length of the 'searchHistory' is greater than 5
+  if (searchHistory.length > 5) {
 
-   // Adds a click event listener to the searchHistoryEntry button.
-   searchHistoryEntry.addEventListener("click", function() {
-     city = this.innerText;
-     var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-     fetchWeather(apiURL);
-   });
-   searchHistoryContainer.insertBefore(searchHistoryEntry, searchHistoryContainer.firstChild);
- 
-   var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-   fetchWeather(apiURL);
- }
- 
- // Function to fetch weather data
- function fetchWeather(apiURL) {
-   fetch(apiURL)
-     .then(response => response.json())
-     .then(data => {
-       var longitude = data.coord.lon;
-       var latitude = data.coord.lat;
-       var temperature = (data.main.temp - 273.15) * 9 / 5 + 32;
-       var wind = data.wind.speed;
-       var humidity = data.main.humidity;
- 
-       displayData(temperature, wind, humidity);
- 
-       var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${APIKey}`;
-       fetch(forecastURL)
-         .then(response => response.json())
-         .then(data => {
-           var forecast = data.list;
-           displayFiveDayForecast(forecast);
-         })
-         .catch(error => console.log(error));
-     })
-     .catch(error => console.log(error));
- }
- 
- function displayFiveDayForecast(forecast) {
-   var forecastContainer = document.getElementById("forecast-container");
-   forecastContainer.innerHTML = "";
- 
-   var uniqueDates = []; // Array to store unique dates
- 
-   forecast.forEach(forecastData => {
-     var dateTime = new Date(forecastData.dt_txt);
-     var date = dateTime.toLocaleDateString("en-US");
- 
-     if (!uniqueDates.includes(date) && uniqueDates.length < 5) {
-       uniqueDates.push(date);
- 
-       var temperature = (forecastData.main.temp - 273.15) * 9 / 5 + 32;
-       var wind = forecastData.wind.speed;
-       var humidity = forecastData.main.humidity;
-       var weatherIcon = forecastData.weather[0].icon;
-       var weatherCode = forecastData.weather[0].id;
- 
-       var forecastEntry = document.createElement("div");
-       forecastEntry.classList.add("forecast-entry");
- 
-       var dateElement = document.createElement("p");
-       dateElement.innerText = date;
- 
-       var temperatureElement = document.createElement("p");
-       temperatureElement.innerText = temperature;
- 
-       var windElement = document.createElement("p");
-       windElement.innerText = "Wind: " + wind;
- 
-       var humidityElement = document.createElement("p");
-       humidityElement.innerText = "Humidity: " + humidity;
- 
-       var weatherIconElement = document.createElement("i");
-       weatherIconElement.classList.add("wi", "wi-owm-" + weatherIcon);
- 
-       var weatherEmojiElement = document.createElement("span");
-       weatherEmojiElement.classList.add("weather-emoji");
-       var weatherEmoji = getWeatherEmoji(weatherCode);
-       weatherEmojiElement.innerText = weatherEmoji;
- 
-       forecastEntry.appendChild(dateElement);
-       forecastEntry.appendChild(temperatureElement);
-       forecastEntry.appendChild(windElement);
-       forecastEntry.appendChild(humidityElement);
-       forecastEntry.appendChild(weatherIconElement);
-       forecastEntry.appendChild(weatherEmojiElement);
- 
-       forecastContainer.appendChild(forecastEntry);
-     }
-   });
- }
- 
- // Function to display search history
- function displaySearchHistory() {
-   var searchHistoryContainer = document.getElementById("search-history");
-   searchHistoryContainer.innerHTML = "";
- 
-   for (var i = 0; i < searchHistory.length; i++) {
-     var searchHistoryEntry = document.createElement("button");
-     searchHistoryEntry.innerText = searchHistory[i];
-     searchHistoryEntry.addEventListener("click", function() {
-       city = this.innerText;
-       var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-       fetchWeather(apiURL);
-     });
-     searchHistoryContainer.appendChild(searchHistoryEntry);
-   }
- }
- 
- // Function to display weather data on the page
- function displayData(temperature, wind, humidity) {
-   document.getElementById("temperature").innerText = temperature + " °F";
-   document.getElementById("wind").innerText = wind;
-   document.getElementById("humidity").innerText = humidity;
- }
- 
 
- // Helper function to map weather codes to emojis
+// Removes the last element from the searchHistory array if its length is greater than 5, 
+// keeping only the most recent 5 search entries.
+  searchHistory.pop();
+}
+// Save search history to local storage
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+
+// Created a new button element and assigned it to the var 'searchHistoryEntry'
+  var searchHistoryEntry = document.createElement("button");
+
+// Setting text content of the searchHistoryEntry button to the input value
+  searchHistoryEntry.innerText = input;
+
+// Adds a click event listener to the searchHistoryEntry button.
+  searchHistoryEntry.addEventListener("click", function() {
+
+// this assigns the text content of the clicked button to the var city
+    city = this.innerText;
+
+//  created variable apiURL using the city and APIKey variables to fetch weather data.
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+
+// calling the fetchWeather function with the apiURL 
+  fetchWeather(apiURL);
+});
+
+// inserting
+  searchHistoryContainer.insertBefore(searchHistoryEntry, searchHistoryContainer.firstChild);
+
+  var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+  fetchWeather(apiURL);
+}
+
+// Declares a function named fetchWeather that takes an apiURL parameter.
+// This function is responsible for fetching weather data from the provided API URL.
+function fetchWeather(apiURL) {
+
+// Initiates a network request to the API URL and returns a promise.
+  fetch(apiURL)
+
+// chains a promise callback that converts the response to JSON format.
+  .then(response => response.json())
+
+//Chains a promise callback that receives the JSON data retrieved from 
+  .then(data => {
+
+// extracts the longitude value from the API response and assigns it to the longitude variable.
+  var longitude = data.coord.lon;
+
+// extracts the latitude value from the API response and assigns it to the latitude variable.
+  var latitude = data.coord.lat;
+
+// Calculates the temperature in Fahrenheit using the temperature value from the API response 
+// and assigns it to the temperature variable.
+  var temperature = (data.main.temp - 273.15) * 9 / 5 + 32;
+
+//  extracts the wind speed value from the API response and assigns it to the wind variable.
+  var wind = data.wind.speed;
+
+// Extracts the humidity value from the API response and assigns it to the humidity variable.
+  var humidity = data.main.humidity;
+
+// calls the displayData function to display the retrieved weather data on the page.
+  displayData(temperature, wind, humidity);
+
+//Constructs the forecast API URL using the latitude, longitude, and APIKey variables to fetch the five-day forecast data.
+  var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${APIKey}`;
+
+// Initiates a network request to the forecast API URL and returns a promise.
+  fetch(forecastURL)
+
+// Chains a promise callback that converts the response to JSON format.
+  .then(response => response.json())
+
+// Chains a promise callback that receives the JSON data retrieved from the forecast API.
+  .then(data => {
+
+// Extracts the forecast data from the API response and assigns it to the forecast variable.
+  var forecast = data.list;
+
+// Calls the displayFiveDayForecast function to display the five-day forecast data on the page.
+displayFiveDayForecast(forecast);
+})
+
+// console logging any errors  
+  .catch(error => console.log(error));
+})
+  .catch(error => console.log(error));
+}
+
+// Declares a function named displayFiveDayForecast that takes the forecast parameter.
+// This function is responsible for displaying the five-day forecast data on the page.
+function displayFiveDayForecast(forecast) {
+
+// DOM element with the ID "forecast-container" and assigns it to the variable forecastContainer.
+var forecastContainer = document.getElementById("forecast-container");
+// Clears the content of the forecastContainer by setting its innerHTML to an empty string.
+forecastContainer.innerHTML = "";
+//  Declares an empty array variable uniqueDates to store unique dates for filtering the forecast data.
+var uniqueDates = []; 
+
+// Iterates over each element in the forecast array using a forEach loop
+forecast.forEach(forecastData => {
+// Converts the date string from the forecast data into a JavaScript Date object and assigns it to the dateTime variable.
+var dateTime = new Date(forecastData.dt_txt);
+//  Converts the dateTime to a locale-specific date string and assigns it to the date variable.
+var date = dateTime.toLocaleDateString("en-US");
+
+// Checks if the date is not already present in the uniqueDates array and the number of unique dates is less than 5.
+if (!uniqueDates.includes(date) && uniqueDates.length < 5) {
+// Adds the date to the uniqueDates array.
+uniqueDates.push(date);
+ 
+// Calculates the temperature in Fahrenheit using the temperature value from the forecast data
+// and assigns it to the temperature variable.
+  var temperature = (forecastData.main.temp - 273.15) * 9 / 5 + 32;
+// Extracts the wind speed value from the forecast data and assigns it to the wind variable.
+  var wind = forecastData.wind.speed;
+// Extracts the humidity value from the forecast data and assigns it to the humidity variable.
+  var humidity = forecastData.main.humidity;
+// Extracts the weather icon value from the forecast data and assigns it to the weatherIcon variable.
+  var weatherIcon = forecastData.weather[0].icon;
+// Extracts the weather code value from the forecast data and assigns it to the weatherCode variable.
+  var weatherCode = forecastData.weather[0].id;
+
+// Creates a new div element and assigns it to the forecastEntry variable.
+// Adds the CSS class "forecast-entry" to the forecastEntry div element.
+  var forecastEntry = document.createElement("div");
+  forecastEntry.classList.add("forecast-entry");
+
+// Creates a new paragraph element and assigns it to the dateElement variable.
+// Sets the text content of the dateElement paragraph to the date value.
+  var dateElement = document.createElement("p");
+  dateElement.innerText = date;
+
+// Creates a new paragraph element and assigns it to the temperatureElement variable.
+// Sets the text content of the temperatureElement paragraph to the temperature value.
+  var temperatureElement = document.createElement("p");
+  temperatureElement.innerText = temperature;
+
+// Creates a new paragraph element and assigns it to the windElement variable.
+// Sets the text content of the windElement paragraph to the concatenation of the string "Wind: " and the wind value.
+  var windElement = document.createElement("p");
+  windElement.innerText = "Wind: " + wind;
+
+// Creates a new paragraph element and assigns it to the humidityElement variable.
+// Sets the text content of the humidityElement paragraph to the concatenation of the string "Humidity: " and the humidity value.
+  var humidityElement = document.createElement("p");
+  humidityElement.innerText = "Humidity: " + humidity;
+
+// Creates a new i element and assigns it to the weatherIconElement variable.
+// Adds CSS classes "wi" and "wi-owm-" concatenated with the weatherIcon value to the weatherIconElement.
+  var weatherIconElement = document.createElement("i");
+  weatherIconElement.classList.add("wi", "wi-owm-" + weatherIcon);
+
+// Creates a new span element and assigns it to the weatherEmojiElement variable.
+var weatherEmojiElement = document.createElement("span");
+// Adds the CSS class "weather-emoji" to the weatherEmojiElement span.
+weatherEmojiElement.classList.add("weather-emoji");
+// Calls the getWeatherEmoji function with the weatherCode value and assigns the returned emoji to the weatherEmoji variable.
+var weatherEmoji = getWeatherEmoji(weatherCode);
+// Sets the text content of the weatherEmojiElement span to the weatherEmoji value.
+weatherEmojiElement.innerText = weatherEmoji;
+
+// appending all these elements as a child 
+forecastEntry.appendChild(dateElement);
+forecastEntry.appendChild(temperatureElement);
+forecastEntry.appendChild(windElement);
+forecastEntry.appendChild(humidityElement);
+forecastEntry.appendChild(weatherIconElement);
+forecastEntry.appendChild(weatherEmojiElement);
+
+// appends the forecastEntry div as a child of the forecastContainer element.
+forecastContainer.appendChild(forecastEntry);
+}
+});
+}
+
+// Declares a function named displaySearchHistory responsible for displaying the search history on the page
+function displaySearchHistory() {
+// Retrieves the DOM element with the ID "search-history" and assigns it to the variable searchHistoryContainer.
+  var searchHistoryContainer = document.getElementById("search-history");
+// Clears the content of the searchHistoryContainer by setting its innerHTML to an empty string.
+  searchHistoryContainer.innerHTML = "";
+// Retrieve search history from local storage
+  var storedSearchHistory = localStorage.getItem("searchHistory");
+  if (storedSearchHistory) {
+  searchHistory = JSON.parse(storedSearchHistory);
+  }
+// Iterates over the elements in the searchHistory array using a for loop.
+  for (var i = 0; i < searchHistory.length; i++) {
+// Creates a new button element and assigns it to the searchHistoryEntry variable.
+  var searchHistoryEntry = document.createElement("button");
+// Sets the text content of the searchHistoryEntry button to the value of searchHistory at index i.
+  searchHistoryEntry.innerText = searchHistory[i];
+//  Adds a click event listener to the searchHistoryEntry button
+  searchHistoryEntry.addEventListener("click", function() {
+// Assigns the text content of the clicked button to the variable city
+  city = this.innerText;
+// Constructs the API URL using the city and APIKey variables to fetch weather data.
+  var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+// Calls the fetchWeather function with the constructed API URL.
+  fetchWeather(apiURL);
+});
+// Appends the searchHistoryEntry button as a child of the searchHistoryContainer element.
+  searchHistoryContainer.appendChild(searchHistoryEntry);
+}
+}
+
+// Call the displaySearchHistory function when the page loads
+window.addEventListener("load", displaySearchHistory);
+
+ // Declares a function named displayData responsible for displaying the weather data on the page.
+function displayData(temperature, wind, humidity) {
+// Sets the text content of the element with the ID "temperature" to the temperature value followed by the string " °F".
+  document.getElementById("temperature").innerText = temperature + " °F";
+// Sets the text content of the element with the ID "wind" to the wind value.
+  document.getElementById("wind").innerText = wind;
+// Sets the text content of the element with the ID "humidity" to the humidity value.
+  document.getElementById("humidity").innerText = humidity;
+}
+
+
+// Helper function to map weather codes to emojis
+// Declares a function named getWeatherEmoji that takes a weatherCode parameter
+// This function maps weather codes to corresponding emojis.
+// Declares and initializes an object emojiMap that maps weather codes to emojis.
  function getWeatherEmoji(weatherCode) {
-   var emojiMap = {
+  var emojiMap = {
      200: "⛈️", // Thunderstorm
      300: "🌦️", // Drizzle
      500: "🌧️", // Rain
@@ -170,15 +269,15 @@
      802: "⛅", // Scattered clouds
      803: "🌥️", // Broken clouds
      804: "☁️" // Overcast clouds
-   };
- 
-   return emojiMap[weatherCode] || "";
- }
- 
- // Event listener for search form submission
- document.getElementById("search-form").addEventListener("submit", search);
- 
- // Call the displaySearchHistory function when the page loads
- window.addEventListener("load", displaySearchHistory);
- 
- 
+};
+
+// Returns the emoji corresponding to the provided weatherCode from the emojiMap object
+// If the code is not found, an empty string is returned.
+return emojiMap[weatherCode] || "";
+}
+
+// submit event listener to the element with the ID "search-form" that triggers the search function when the form is submitted.
+document.getElementById("search-form").addEventListener("submit", search);
+
+ // Adds a load event listener to the window object that triggers the displaySearchHistory function when the page finishes loading.
+window.addEventListener("load", displaySearchHistory);
